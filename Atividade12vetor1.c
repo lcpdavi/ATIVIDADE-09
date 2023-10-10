0c0e53f4f64554056f6827e9b0bcb300478d3888
Davi da Costa Silva Barros <143516434+lcpdavi@users.noreply.github.com>
2023-10-10 09:35:42 -0300

    Update Atividade12vetor1.c

--- Atividade12vetor1.c
+++ Atividade12vetor1.c
@@ -11,11 +11,9 @@ int main() {
     soma += vetor[i]; // Adicionar o valor atual à soma
 
     if (i == 0) {
-      // Inicializar maiorValor e menorValor com o primeiro elemento do vetor
       maiorValor = vetor[i];
       menorValor = vetor[i];
     } else {
-      // Atualizar maiorValor e menorValor, se necessário
       if (vetor[i] > maiorValor) {
         maiorValor = vetor[i];
       }
@@ -24,16 +22,10 @@ int main() {
       }
     }
   }
-
-  // Calcular a média dos valores
   mediaValores = soma / 5;
-
-  // Mostrar os valores do vetor
   for (int i = 0; i < 5; i++) {
     printf("vetor[%d] = %d\n", i, vetor[i]);
   }
-
-  // Mostrar os resultados
   printf("Maior Valor: %f\n", maiorValor);
   printf("Menor Valor: %f\n", menorValor);
   printf("Média dos Valores: %f\n", mediaValores);
