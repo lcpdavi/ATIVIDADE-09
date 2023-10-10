a988036789e33e96628d7498f3d1448ee6948d4d
Davi da Costa Silva Barros <143516434+lcpdavi@users.noreply.github.com>
2023-10-10 10:15:56 -0300

    Update Atividade13vetor.c

--- Atividade13vetor.c
+++ Atividade13vetor.c
@@ -6,10 +6,7 @@ int main(){
     for(int i = 0; i < 5; i++){
         printf("Digite os valores:");
         scanf("%d", &vetor[i]);
-
-
     }
-
     maior = vetor[0];
     menor = vetor[0];
 
@@ -17,15 +14,12 @@ int main(){
         if(vetor[i] > maior){
             maior = vetor[i];
             pMaior = i;
-
         }
         if(vetor[i] < menor){
             menor = vetor[i];
             pMenor = i;
         }
     }
-
     printf("maior: %d, Posicao: %d \nmenor: %d, Posicao: %d", maior, 
-     pMaior, menor, pMenor);
-
+    pMaior, menor, pMenor);
 }
